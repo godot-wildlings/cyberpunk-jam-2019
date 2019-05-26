b7a56c209e05e89dc2110e25a94b043746c69860
Kevin von Spiczak <caevv@me.com>
2019-05-26 18:26:15 +0200

    fix warning, add Gonkee's fog shader

--- Ghost/Ghost.gd
+++ Ghost/Ghost.gd
@@ -7,5 +7,5 @@ func _ready():
 	alpha_tween.interpolate_property(self, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), .6, Tween.TRANS_SINE, Tween.EASE_OUT)
 	alpha_tween.start()
 
-func _on_AlphaTween_tween_completed(obj : Object, key : int):
+func _on_AlphaTween_tween_completed(obj : Object, key : NodePath):
 		queue_free()
